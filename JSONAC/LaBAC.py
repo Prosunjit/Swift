532b5fea935fa675819d55b7241767e9b9b410e2
Prosunjit Biswas <prosun.csedu@gmail.com>
2014-08-23 22:24:52 +0000

    object hierarchy is working

--- JSONAC/LaBAC.py
+++ JSONAC/LaBAC.py
@@ -5,6 +5,7 @@
 class ObjectLabel(object):
 	
 	def __init__(self, name):
+		self.name = name
 		# cleared_label  means the user label that have access this object label  as specified in the policy.
 		self.cleared_u_label = []
 		# inferred_label means inferred user labels that have access to this object label by user label hierarchy
@@ -17,9 +18,25 @@ class ObjectLabel(object):
         def is_senior_to(self):
                 return self.junior_labels
 
+	# return set of direct junior roles
 	@is_senior_to.setter	
 	def is_senior_to(self,label):
 		self.junior_labels.append(label)
+		
+	# find all junior labels to this label
+	def all_junior_labels(self):
+		juniors =  self._all_junior_labels()
+		if self.name in juniors:
+			juniors.remove(self.name)
+		return juniors
+	
+	def _all_junior_labels(self):
+		res = []
+		for l in iter(self.is_senior_to):
+			res += l._all_junior_labels()
+		# assuming a node is junior to itself
+		return res + [self.name]
+		
 
 	@property
 	def acl(self):
@@ -81,6 +98,71 @@ class UserLabel(object):
 			pass
 
 
+'''
+	access label hierarchy. This is essentially a partial order.
+
+'''
+
+class LabelHierarchy:
+	def __init__(self, user=False):
+		# all the root of the partial order set is stored in root_list
+		self.root_list = []
+		# all labels of the Label Hierarchy
+		self.labels=[]
+		# if object_type = True , it means it is Object_label hieararchy. otherwise userlabel hierarchy
+		self.object_type = True
+		if user == True:
+			self.object_type = False
+
+	def _default_hierarchy_setup(self):
+
+		self.add_x_dominates_y(x="private",y="public")
+		self.add_x_dominates_y(x="protected",y="private")
+
+	def add_x_dominates_y(self, x=None, y=None): # insert a hierarchy of two labels such that x dominates y. x_v, y_v are values of x & y
+		if self._find_node(x) == None:
+			self._add_2_nodes(x)
+		if self._find_node(y) == None:
+			self._add_2_nodes(y)
+
+		xx = self._find_node(x)
+		yy = self._find_node(y)
+		#self._add_x_dominates_y(x=xx, y=yy)
+		xx.is_senior_to = yy
+		
+	def _add_x_dominates_y(x=None, y=None):
+		#if self.object_type == True:
+		x.is_senior_to = y
+
+
+	def _add_2_nodes(self,x_v):
+		if self.object_type == True:
+			tn = ObjectLabel(x_v)
+		else:
+			tn = UserLabel(x_v)
+		self.labels.append(tn)
+		
+	
+	def _find_node(self,x_v):
+		
+		for n in self.labels:
+			if n.name == x_v:
+				return n
+		return None
+	
+	def get_hierarchy(self):
+		res = []
+		for label in iter(self.labels):
+			t =  (label.name, label.all_junior_labels())
+			res.append(t)
+		return res
+			
+
+def test_object_hierarchy():
+	olh = LabelHierarchy()
+	olh.add_x_dominates_y(x="o1",y="o2")
+	olh.add_x_dominates_y(x="o2",y="o3")
+	print olh.get_hierarchy()
 
 def test_object_label():
 	ol = ObjectLabel("o3")
@@ -101,4 +183,4 @@ def test():
 	test_object_label()
 
 if __name__ == "__main__":
-	test()
+	test_object_hierarchy()
